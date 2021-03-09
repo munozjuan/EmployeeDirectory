26bcd5f7aa2a25b961d7a29e528689b5a6aef463
munozjuan <munoz.juan07@hotmail.com>
2021-03-08 23:12:04 -0500

    adding name, phone email

--- src/components/container/EmployeesContainer.jsx
+++ src/components/container/EmployeesContainer.jsx
@@ -2,3 +2,19 @@ import React, { Component } from "react";
 import SearchBar from "../SearchBar";
 import EmployeeTable from "../EmployeeTable";
 import API from "../../utils/API";
+
+class EmployeesContainer extends Component {
+    state = {
+      search: "",
+      employees: [],
+      filteredEmployees: [],
+      sortDirections: this.initialSortDirections,
+    };
+    get initialSortDirections() {
+        return {
+          name: "",
+          phone: "",
+          email: "",
+          dob: "",
+        };
+      }
