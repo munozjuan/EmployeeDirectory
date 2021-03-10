c57eb9114028417342abb98237fbd2193a22ca9a
munozjuan <munoz.juan07@hotmail.com>
2021-03-09 20:26:11 -0500

    fixing bug

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const URL = "https://randomuser.me/api/?results=50&nat=us"
+// eslint-disable-next-line
 export default {
     getEmployees:function(){
         return axios.get(URL);
